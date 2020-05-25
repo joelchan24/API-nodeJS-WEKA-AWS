86ac288d984ee80e635277ed4c41c699e825c2d0
joel <joelchan1220@gmail.com>
2020-05-25 17:30:20 -0500

    cambios

--- mainServidorREST.js
+++ mainServidorREST.js
@@ -5,19 +5,23 @@
 // .....................................................................
 const express = require( 'express' )
 const bodyParser = require( 'body-parser' )
+var cors = require('cors')
+
 // .....................................................................
 // .....................................................................
 function main() {
   // creo el servidor
   var servidorExpress = express()
   // para poder acceder a la carga de la petición http, asumiendo que es JSON
+  servidorExpress.use(cors()) // Use this after the variable declaration
+
   servidorExpress.use ( bodyParser.text({type : 'application/json'}) )
   // cargo las reglas REST
   var reglas = require( "./ReglasREST.js")
   reglas.cargar( servidorExpress )
   // arranco el servidor
-  var servicio = servidorExpress.listen( 80, function() {
-    console.log( "servidor REST escuchando en el puerto 80 ")
+  var servicio = servidorExpress.listen( 3000, function() {
+    console.log( "servidor REST escuchando en el puerto 3000 ")
   })
 
   // capturo control-c para cerrar el servicio ordenadamente
